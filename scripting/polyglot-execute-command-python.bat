8a1640b2af85ad2b53a12e2493b6de1ef8647fdd
Isaac Rex <isaaclrex@gmail.com>
2026-06-12 14:06:23 -1000

    Fix CRONOS_KIKIT escape

--- scripting/polyglot-execute-command-python.bat
+++ scripting/polyglot-execute-command-python.bat
@@ -1,7 +1,7 @@
 echo \" <<'BATCH_SCRIPT' >/dev/null ">NUL "\" \`" <#"
 @ECHO OFF
 REM ===== Batch Script Begin =====
-call "%CRONOS_KIKIT_ROOT_DIR%\scripting\execute-command-python.bat" %*
+call "%IREX_KIKIT_ROOT_DIR%\scripting\execute-command-python.bat" %*
 exit /b %ERRORLEVEL%
 REM ====== Batch Script End ======
 GOTO :eof
@@ -12,7 +12,7 @@ BATCH_SCRIPT
 
 set +o histexpand 2>/dev/null
 # ===== Bash Script Begin =====
-exec "$CRONOS_KIKIT_ROOT_DIR/scripting/execute-command-python.sh" "$@"
+exec "$IREX_KIKIT_ROOT_DIR/scripting/execute-command-python.sh" "$@"
 # ====== Bash Script End ======
 case $- in *"i"*) cat /dev/stdin >/dev/null ;; esac
 exit
